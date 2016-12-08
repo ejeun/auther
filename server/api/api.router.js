8836ebaecc33dbd92cff109c901dd65f5cf0827c
jeunlee <junniesu@gmail.com>
2016-12-08 18:30:59 -0500

    end of thursday

--- server/api/api.router.js
+++ server/api/api.router.js
@@ -3,7 +3,7 @@
 var router = require('express').Router();
 
 router.use('/users', require('./users/user.router'));
-
 router.use('/stories', require('./stories/story.router'));
+router.use('/login', require('./login/login.router'));
 
 module.exports = router;
---
+++ server/api/login/login.router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var router = require('express').Router();
+
+var HttpError = require('../../utils/HttpError');
+
+router.post('/', function (req, res, next){
+  User.findAll({
+    where:
+  })
+})
